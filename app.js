7ff51c88a840e8d623f663b7202fd7ece24a0f53
raziullah-khann <raziullahkhan25@gmail.com>
2025-02-27 15:13:17 +0530

    fix issue

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
-const express = require('expresss');
-const bodyParser = require('body-parser');
-
-const app = express();
-
-app.listen(3000, )
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(3000, () => {
+    console.log("server start on port 3000");
+});
